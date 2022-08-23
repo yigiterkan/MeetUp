369dd46685e1ed8eb807b863394b720f28485104
Yiğit Cumhur Erkan <erkan.cumhur@hotmail.com>
2022-08-23 13:49:02 +0300

    First commit

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,8 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  function addMeetupHandler() {}
+  return <NewMeetupForm onAddMeetUp={addMeetupHandler} />;
+}
+
+export default NewMeetupPage;
